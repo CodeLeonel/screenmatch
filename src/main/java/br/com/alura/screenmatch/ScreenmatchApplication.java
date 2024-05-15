2b0e78f625ec454db36cb56f82e9f92fa449489e
Hugo Leonel <hugoleonel.code@gmail.com>
2024-05-15 18:28:46 -0300

    DadosSerie e ConverteDados

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,9 +19,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&season=1&apikey=616bb6bc");
+		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=616bb6bc");
 		System.out.println(json);
-		
+		var converteDados = new ConverteDados();
+		var objeto = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(objeto);
 	}
 	
 	
